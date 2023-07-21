6f2135428d86098212eb84004032727bb79e7707
sanadharwish <sanadharwish@gmail.com>
2023-07-21 16:47:45 +0530

    redux-persist edited

--- sports mgt/Frontend/vite.config.js
+++ sports mgt/Frontend/vite.config.js
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:3000,
-  }
-})
-
-
-
+  server: {
+    port: 3000,
+  },
+  build: {
+    rollupOptions: {
+      external: ['redux-persist/integration/react'],
+    },
+  },
+});
