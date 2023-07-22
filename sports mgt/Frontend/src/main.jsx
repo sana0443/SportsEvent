6213abecbc75538cef69c01c9f5b60f270c6e34a
sanadharwish <sanadharwish@gmail.com>
2023-07-22 05:53:15 +0530

    main.jsx updated

--- sports mgt/Frontend/src/main.jsx
+++ sports mgt/Frontend/src/main.jsx
@@ -1,18 +1,40 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { createRoot } from 'react-dom/client';
+// import { Provider } from 'react-redux';
+// import {store,persistor} from './Redux/Store';
+// import App from './App';
+// import './index.css';
+// import { PersistGate } from 'redux-persist/integration/react';
+
+// // Assuming you have a configured Redux store
+
+// createRoot(document.getElementById('root')).render(
+//   <Provider store={store}>
+//     <PersistGate loading={null} persistor={persistor}>
+//     <App />
+//     </PersistGate>
+//   </Provider>
+// );
+
+
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import {store,persistor} from './Redux/Store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './Redux/Store';
 import App from './App';
 import './index.css';
-import { PersistGate } from 'redux-persist/integration/react';
 
-// Assuming you have a configured Redux store
-
-createRoot(document.getElementById('root')).render(
+// Render the App component with Redux Provider and PersistGate
+ReactDOM.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-    <App />
+      <App />
     </PersistGate>
-  </Provider>
+  </Provider>,
+  document.getElementById('root')
 );
+
