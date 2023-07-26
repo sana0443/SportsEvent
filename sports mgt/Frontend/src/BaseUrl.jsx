4cccb2df1a5aa065cef2c287d6ef9267c79b6e2b
sanadharwish <sanadharwish@gmail.com>
2023-07-26 13:59:58 +0530

    baseurl uncommented

--- sports mgt/Frontend/src/BaseUrl.jsx
+++ sports mgt/Frontend/src/BaseUrl.jsx
@@ -1,5 +1,5 @@
-//  const BaseUrl='https://sports-event-yiqz.onrender.com'
+ const BaseUrl='https://sports-event-yiqz.onrender.com'
 
-const BaseUrl='http://localhost:8000'
+// const BaseUrl='http://localhost:8000'
  export default BaseUrl;
  
